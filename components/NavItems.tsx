a1d1cc73bb0be697f5a79367f0177fa7f6bebb5e
kaoutarel <kaoutar.elazzab@gmail.com>
2025-06-24 18:55:11 -0400

    implement navbar

---
+++ components/NavItems.tsx
@@ -0,0 +1,30 @@
+"use client"; // bcs it's rendered on the client side
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Companions", href: "/companions" },
+  { label: "My journey", href: "/my-journey" },
+];
+
+const NavItems = () => {
+  //use pathname hook : to be able to know which of the navitems are we currently on (active)
+  const pathname = usePathname();
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link
+          href={href}
+          key={label}
+          className={cn(pathname === href && "text-primary font-semibold")}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default NavItems;

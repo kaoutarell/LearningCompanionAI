1b19ff0ae76e29936820433d846673cd95919ec2
kaoutarel <kaoutar.elazzab@gmail.com>
2025-09-11 22:59:47 -0400

    implement subject filtering (new component)

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,10 +1,63 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { subjects } from "@/constants";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
 
 const SubjectFilter = () => {
-  return (
-    <div>
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("subject") || "";
 
-    </div>
+  // we need a useState to modify it :
+  const [subject, setSubject] = useState("");
+
+  useEffect(() => {
+    let newUrl = "";
+    if (subject === "all") {
+      newUrl = removeKeysFromUrlQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["subject"],
+      });
+    } else {
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "subject",
+        value: subject,
+      });
+    }
+    router.push(newUrl, { scroll: false });
+  }, [subject]);
+
+  return (
+    <Select onValueChange={setSubject} value={subject}>
+      <SelectTrigger className="input capitalize">
+        <SelectValue placeholder="Subject" />
+      </SelectTrigger>
+      <SelectContent>
+        {subjects.map((subject) => (
+          <SelectItem value={subject} key={subject} className="capitalize">
+            {subject}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 };
 

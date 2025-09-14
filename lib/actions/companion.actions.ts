d759942fdefd6431a3e4b19db8b5ce37ddf467e8
kaoutarel <kaoutar.elazzab@gmail.com>
2025-09-13 21:47:55 -0400

    set up the sessions history

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -65,3 +65,47 @@ export const getCompanion = async (id: string) => {
 
   return data[0];
 };
+
+//add to session history
+export const addToSessionHistory = async (companionId: string) => {
+  const { userId } = await auth();
+  const supabase = createSuperbaseClient();
+
+  const { data, error } = await supabase
+    .from("session_history")
+    .insert({ companion_id: companionId, user_id: userId });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+};
+
+//recent sessions
+export const getRecentSessions = async (limit = 10) => {
+  const supabase = createSuperbaseClient();
+  const { data, error } = await supabase
+    .from("session_history")
+    .select(`companions:companion_id(*)`)
+    .order("created_at", { ascending: false })
+    .limit(10); //from newest to oldest
+
+  if (error) throw new Error(error.message);
+
+  //destructure the data
+  return data.map(({ companions }) => companions);
+};
+
+export const getUserSessions = async (userId: string, limit = 10) => {
+  const supabase = createSuperbaseClient();
+  const { data, error } = await supabase
+    .from("session_history")
+    .select(`companions:companion_id(*)`)
+    .eq("user_id", userId)
+    .order("created_at", { ascending: false })
+    .limit(10); //from newest to oldest
+
+  if (error) throw new Error(error.message);
+
+  //destructure the data
+  return data.map(({ companions }) => companions);
+};
